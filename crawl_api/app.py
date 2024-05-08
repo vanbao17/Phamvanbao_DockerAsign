a859253e53be1e7dc2ebcd51eecb99e739c2b8db
vanbao17 <vanpao171103@gmail.com>
2024-05-08 12:53:49 +0700

    final commit

--- crawl_api/app.py
+++ crawl_api/app.py
@@ -5,21 +5,23 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json as js
 import logging
+import schedule
+import time
 app = Flask(__name__)
 CORS(app)
-
-# Giả sử rằng 'api' là một module bạn tự định nghĩa chứa hàm fetch_books
 from api import fetch_books
-
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_dir)
-
+logger = logging.getLogger(__name__)
 @app.route('/crawl', methods=['GET'])
-def crawl_data():
+def crawl():
+    logger.debug("đang chạy")
     try:
         books = fetch_books()
-        logging.debug(f"Found data:{books}")
+        logger.debug(f"Found data:{books}")
     except:
+        logger.error("Error fetching books data: {}".format(e))
         return jsonify({"error": "du lieu fetch sai roi"}), 500
     
     try:
@@ -28,12 +30,12 @@ def crawl_data():
         return jsonify({"error": "khong ket noi duo vowi db"}), 500
     try:
         if response.status_code == 200:
-            return jsonify({"message": "Data inserted successfully"}), 200
+            return jsonify({"message": "Data inserted successfully","data":books}), 200
         else:
             return jsonify({"error": "Failed to insert data: " }), 500
     except Exception as e:
         return jsonify({"error": "An error occurred during data insertion"}), 500
 
-
+schedule.every().day.at("02:35").do(crawl)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
